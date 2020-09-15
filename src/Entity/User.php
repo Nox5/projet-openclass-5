ae5518982d92b8f84ec7032f7ca6334f1b692010
Jean-Philippe <jp.desmet1@gmail.com>
2020-09-15 21:00:40 +0200

    mise à niveau master

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class Users implements UserInterface
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -35,14 +37,9 @@ class Users implements UserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="boolean")
      */
-    private $name;
-
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $firstname;
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -122,26 +119,14 @@ class Users implements UserInterface
         // $this->plainPassword = null;
     }
 
-    public function getName(): ?string
+    public function isVerified(): bool
     {
-        return $this->name;
+        return $this->isVerified;
     }
 
-    public function setName(string $name): self
+    public function setIsVerified(bool $isVerified): self
     {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
+        $this->isVerified = $isVerified;
 
         return $this;
     }
