21b6df60a56ca37800e69686d8f37272f2760bca
Jean-Philippe <jp.desmet1@gmail.com>
2020-09-15 20:43:41 +0200

    mise en place formulaire pour chrnonique commentaire

--- src/Controller/ChroniqueController.php
+++ src/Controller/ChroniqueController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Chronique;
+use App\Entity\Commentaire;
+use App\Form\CommentaireFormType;
 use App\Form\FormChroniqueType;
 use App\Repository\ChroniqueRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use PhpParser\Builder\Property;
 use ProxyManager\ProxyGenerator\Util\Properties;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,18 +18,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChroniqueController extends AbstractController
 {
     /**
-     * @Route("/", name="chronique")
+     * @Route("/chronique/{id}", name="chronique")
      */
-    public function index(ChroniqueRepository $chroniqueRepository)
+    public function index(Request $request, Chronique $chronique, EntityManagerInterface $entityManager)
     {
-        $chroniques = $chroniqueRepository->findAll();
+        $commentaire = new Commentaire();
 
-        $chronique = new Chronique();
+        $form = $this->createForm(CommentaireFormType::class, $commentaire);
 
-        $form = $this->createForm(FormChroniqueType::class, $chronique);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $commentaire->setDate(new DateTime());
+
+            $chronique->addCommentaire($commentaire);
+
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+        }
 
         return $this->render('chronique/chronique.html.twig', [
-            'chroniqueBd' => $chroniques,
+            'chronique' => $chronique,
             'form' => $form->createView(),
         ]);
     }
