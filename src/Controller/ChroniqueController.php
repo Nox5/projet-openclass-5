b7bed4989b3f16a57a85c1b7358060d017ac55dc
Jean-Philippe <jp.desmet1@gmail.com>
2020-09-22 15:58:53 +0200

    CRUD mis en place pour les chroniques

--- src/Controller/ChroniqueController.php
+++ src/Controller/ChroniqueController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Chronique;
 use App\Entity\Commentaire;
+use App\Form\ChroniqueFormType;
 use App\Form\CommentaireFormType;
 use App\Form\FormChroniqueType;
 use App\Repository\ChroniqueRepository;
@@ -13,11 +14,14 @@ use Symfony\Component\HttpFoundation\Request;
 use PhpParser\Builder\Property;
 use ProxyManager\ProxyGenerator\Util\Properties;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChroniqueController extends AbstractController
 {
     /**
+     * Récupération d'une chronique(read)
+     * Permet l'ajout d'un commentaire en base de donnée
      * @Route("/chronique/{id}", name="chronique")
      */
     public function index(Request $request, Chronique $chronique, EntityManagerInterface $entityManager)
@@ -41,5 +45,74 @@ class ChroniqueController extends AbstractController
             'chronique' => $chronique,
             'form' => $form->createView(),
         ]);
+    }
+
+
+    /**
+     * Ajouter une chronique(create)
+     *@Route("/add-chronique", name="add_chronique")
+     * @param Request $request
+     * @return Response
+     */
+    public function addChronique(Request $request): Response
+    {
+        $chronique = new Chronique();
+
+        $form = $this->createForm(ChroniqueFormType::class, $chronique);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($chronique);
+            $entityManager->flush();
+        }
+
+        return $this->render("chronique/chronique-form.html.twig", [
+            "form_title" => "Ajouter une chronique",
+            "form_chronique" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Modifie une chronique(update)
+     * @Route("/modify-chronique/{id}", name="modify_chronique")
+     * @param Request $request
+     * @param integer $id
+     * @return Response
+     */
+    public function modifyChronique(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $chronique = $entityManager->getRepository(Chronique::class)->find($id);
+
+        $form = $this->createForm(ChroniqueFormType::class, $chronique);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->flush();
+        }
+
+        return $this->render("chronique/chronique-form.html.twig", [
+            "form_title" => "Modifier la chronique",
+            "form_chronique" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Supprime la chronique(delete)
+     * @Route("/delete-chronique/{id}", name="delete_chronique")
+     * @param integer $id
+     * @return Response
+     */
+    public function deleteChronique(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $chronique = $entityManager->getRepository(Chronique::class)->find($id);
+        $entityManager->remove($chronique);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("chronique");
     }
 }
